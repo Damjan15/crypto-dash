c272a569072378e1bc4994f0541d44dcf706b8de
Damjan15 <stamenovdamjan5@gmail.com>
2025-07-15 20:03:07 +0200

    feat: refactored URL, showing data on the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ function App() {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const API_URL =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+  const API_URL = import.meta.env.VITE_COINS_API_URL;
 
   useEffect(() => {
     const fetchCoins = async () => {
       try {
-        const res = await fetch(API_URL);
+        const res = await fetch(
+          `${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`
+        );
         if (!res.ok) throw new Error("Failed to fetch data!");
         const data = await res.json();
         setCoins(data);
